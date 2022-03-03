e68f3efbf423a2ecb6a3767a72616bdbecff2e97
veli-sethiya <91125109+veli-sethiya@users.noreply.github.com>
2022-03-03 16:07:04 +0530

    Create Seconds_In_A_Year.py

---
+++ Seconds_In_A_Year.py
@@ -0,0 +1,4 @@
+print(f"there are {365*24*60*60} second in a normal year")
+print(f"there are {366*24*60*60} second in a leap year")
+there are 31536000 second in a normal year
+there are 31622400 second in a leap year
