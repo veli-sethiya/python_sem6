4e7ea753ca85d360289ac00688cc2fa6fd2615fc
veli-sethiya <91125109+veli-sethiya@users.noreply.github.com>
2022-03-03 16:01:36 +0530

    Create All_Datatypes.py

---
+++ All_Datatypes.py
@@ -0,0 +1,20 @@
+String1 = 'Welcome'
+print(String1) 
+
+num1=22
+print(num1)
+
+fnum=22.06
+print(fnum)
+
+c=3-6g
+print(c)
+
+l=[6,24,13,34,85]
+print(l)
+
+t=(11,32,53,84)
+print(t)
+
+d={1:"ID",2:"Name",3:"Dept"}
+print(d[1])
